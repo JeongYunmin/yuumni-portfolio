f7616f0e707892a9ab2068752c73981bc0012297
yuumni <minyunj77@gmail.com>
2024-04-08 03:23:31 +0900

    홈페이지의 에니메이션 효과 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,68 @@
 "use client";
 
-import { useParams } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
+import Image from "next/image";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
 
 export default function RootLayout(props) {
   const { id } = useParams();
+  const pathname = usePathname();
+
+  const arr: number[] = [];
+  const randomArr: number[] = [];
+  const randomSize: number[] = [];
+
+  for (let i = 0; i < 25; i++) {
+    arr.push(i);
+  }
+
+  arr.map(() => {
+    randomArr.push(Math.random() * 25);
+    randomSize.push(Math.random() * (140 - 70) + 70);
+  });
 
   return (
     <html>
       <body>
+        <style>
+          {`
+            @keyframes heartEffect {
+              1% { opacity: 1 }
+              60% { opacity: 0 }
+              100% {opacity: 0; transform: translateY(-160vh) }
+            }
+          `}
+        </style>
+
         {id ? (
           <>{props.children}</>
         ) : (
           <>
             <Header />
             {props.children}
+            {pathname === "/" &&
+              arr.map((i) => (
+                <Image
+                  key={i}
+                  src={"/하트.png"}
+                  width={randomSize[i]}
+                  height={randomSize[i]}
+                  className="heart"
+                  alt="하트 이미지"
+                  style={{
+                    position: "absolute",
+                    left: `${randomArr[i] * 4}vw`,
+                    bottom: "0",
+                    opacity: "0",
+                    animationDelay: `${Math.random() * 2}s`,
+                    animationName: "heartEffect",
+                    animationDuration: "4s",
+                    animationTimingFunction: "Ease-out",
+                  }}
+                />
+              ))}
             <Footer />
           </>
         )}
